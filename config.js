7f2441721720745ef51fbf812a3d58f48c576605
Luiza Saraiva <138792025+luizasaraiva@users.noreply.github.com>
2026-06-26 13:34:59 -0300

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// CONFIGURAÇÕES GERAIS DO SITE
-// Altere aqui dados principais usados pelo site.
-
-const DUTS_CONFIG = {
-  nomeMarca: "DutsNails",
-  whatsapp: "5511966818500",
-  cidade: "São Caetano do Sul / ABC Paulista"
-};
