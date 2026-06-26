aec32290b3b8ad388bad36a8bef00d8faa856b1b
Luiza Saraiva <138792025+luizasaraiva@users.noreply.github.com>
2026-06-26 13:33:25 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 function clienteLogin(e){
   e.preventDefault();
   localStorage.setItem("duts_cliente_logado","sim");
-  window.location.href = "area.html";
+  window.location.href = "cliente-area.html";
 }
 function adminLogin(e){
   e.preventDefault();
@@ -9,16 +9,16 @@ function adminLogin(e){
   const pass = document.getElementById("pass").value;
   if(user === "duda" && pass === "duts123"){
     localStorage.setItem("duts_admin_logado","sim");
-    window.location.href = "painel.html";
+    window.location.href = "studio-painel.html";
   }else{
     document.getElementById("erro").style.display = "block";
   }
 }
 function sairAdmin(){
   localStorage.removeItem("duts_admin_logado");
-  window.location.href = "login.html";
+  window.location.href = "studio-login.html";
 }
 function sairCliente(){
   localStorage.removeItem("duts_cliente_logado");
-  window.location.href = "login.html";
+  window.location.href = "cliente-login.html";
 }
