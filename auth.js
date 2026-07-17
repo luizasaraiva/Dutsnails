21ce57562a9691c935f601dc83bcde73fbd66862
Luiza Saraiva <138792025+luizasaraiva@users.noreply.github.com>
2026-07-17 13:36:15 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,24 +1,4 @@
-function clienteLogin(e){
-  e.preventDefault();
-  localStorage.setItem("duts_cliente_logado","sim");
-  window.location.href = "cliente-area.html";
-}
-function adminLogin(e){
-  e.preventDefault();
-  const user = document.getElementById("user").value;
-  const pass = document.getElementById("pass").value;
-  if(user === "duda" && pass === "duts123"){
-    localStorage.setItem("duts_admin_logado","sim");
-    window.location.href = "studio-painel.html";
-  }else{
-    document.getElementById("erro").style.display = "block";
-  }
-}
-function sairAdmin(){
-  localStorage.removeItem("duts_admin_logado");
-  window.location.href = "studio-login.html";
-}
-function sairCliente(){
-  localStorage.removeItem("duts_cliente_logado");
-  window.location.href = "cliente-login.html";
-}
+function clienteLogin(e){e.preventDefault();localStorage.setItem("vellure_cliente_logado","sim");location.href="cliente-area.html"}
+function adminLogin(e){e.preventDefault();const user=document.getElementById("user").value;const pass=document.getElementById("pass").value;if(user==="vellure"&&pass==="studio123"){localStorage.setItem("vellure_admin_logado","sim");location.href="studio-painel.html"}else document.getElementById("erro").style.display="block"}
+function sairAdmin(){localStorage.removeItem("vellure_admin_logado");location.href="studio-login.html"}
+function sairCliente(){localStorage.removeItem("vellure_cliente_logado");location.href="cliente-login.html"}
