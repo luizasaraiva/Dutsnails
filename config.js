a564a6dbe3bd37e78872b50e94f10e5dd194ae3a
Luiza Saraiva <138792025+luizasaraiva@users.noreply.github.com>
2026-07-17 13:37:58 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const VELLURE_CONFIG = {
+  nomeMarca: "Vellure Studio",
+  whatsapp: "5511966818500",
+  cidade: "Vila Formosa • São Paulo",
+  instagram: "@vellurestudio"
+};
+window.VELLURE_CONFIG = VELLURE_CONFIG;
